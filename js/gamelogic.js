119420e143d723e3e42a5ef6bf2857afa5e18bea
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-20 23:42:03 +0000

    Modified JS so that game begins when you click the start button

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,12 +1,15 @@
-$("button").click(function() {
-	var answer = $( this ).val();
-	var pcPick = Math.random();
-	pcPick = 4*pcPick;
-	pcPick = (Math.floor(pcPick))+1;
-	if(answer == pcPick) {
-		alert("You are correct!");
-	}else{
-		alert("Incorrect, I picked " + pcPick);
-	}
+$("#begin").click(function() {
+	$("button").click(function() {
+		var answer = $( this ).val();
+		var pcPick = Math.random();
+		pcPick = 4*pcPick;
+		pcPick = (Math.floor(pcPick))+1;
+		if(answer == pcPick) {
+			alert("You are correct!");
+		}else{
+			alert("Incorrect, I picked " + pcPick);
+		}
+	});
 });
+
 

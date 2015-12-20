833489e351a485c21ff1b71ab2f9ad208e9a2ca3
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-20 23:31:04 +0000

    Got rid of the textbox so had to modify the JS logic

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,10 +1,9 @@
 $("button").click(function() {
-	var text = $( this ).text();
-  	$( "input" ).val( text );
+	var answer = $( this ).val();
 	var pcPick = Math.random();
 	pcPick = 4*pcPick;
 	pcPick = (Math.floor(pcPick))+1;
-	if(text == pcPick) {
+	if(answer == pcPick) {
 		alert("You are correct!");
 	}else{
 		alert("Incorrect, I picked " + pcPick);

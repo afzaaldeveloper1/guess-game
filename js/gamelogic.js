924f47a9a1807b8e529470d2fde1d87b60813321
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-20 23:45:36 +0000

    Modified the pcPick equation and the alert box

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -2,10 +2,10 @@ $("#begin").click(function() {
 	$("button").click(function() {
 		var answer = $( this ).val();
 		var pcPick = Math.random();
-		pcPick = 4*pcPick;
+		pcPick = 3*pcPick;
 		pcPick = (Math.floor(pcPick))+1;
 		if(answer == pcPick) {
-			alert("You are correct!");
+			alert("You are correct!, I picked " + pcPick + "also");
 		}else{
 			alert("Incorrect, I picked " + pcPick);
 		}

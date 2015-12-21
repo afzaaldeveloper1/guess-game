249fa0f192c1814b4723f30bf75688d5bf00ef3d
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-21 00:21:22 +0000

    Want to end round after 5 tries

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,15 +1,18 @@
+i=1;
 $("#begin").click(function() {
-	$(".game").click(function() {
+    $(".game").click(function() {
+        for(i=1; i<6; i++){
 		var answer = $( this ).val();
-		var pcPick = Math.random();
-		pcPick = 3*pcPick;
-		pcPick = (Math.floor(pcPick))+1;
-		if(answer == pcPick) {
-			alert("You are correct!, I picked " + pcPick + "also");
-		}else{
-			alert("Incorrect, I picked " + pcPick);
-		}
-	});
+			var pcPick = Math.random();
+			pcPick = 3*pcPick;
+			pcPick = (Math.floor(pcPick))+1;
+			if(answer == pcPick) {
+				alert("You are correct!, I picked " + pcPick + "also");
+			}else{
+				alert("Incorrect, I picked " + pcPick);
+			}
+		});
+		i++;
+	}
 });
-
 

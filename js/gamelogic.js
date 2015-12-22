2b2d6efd5fb57e81d88fed15c097b5a31456b2af
Syed Afzaal Hassan <afzaal.hassan.engineer@gmail.com>
2015-12-22 00:10:07 +0000

    Corrected game logic

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,18 +1,36 @@
-i=1;
-$("#begin").click(function() {
-    $(".game").click(function() {
-        for(i=1; i<6; i++){
-		var answer = $( this ).val();
-			var pcPick = Math.random();
-			pcPick = 3*pcPick;
-			pcPick = (Math.floor(pcPick))+1;
-			if(answer == pcPick) {
-				alert("You are correct!, I picked " + pcPick + "also");
-			}else{
-				alert("Incorrect, I picked " + pcPick);
-			}
-		});
-		i++;
-	}
+$("#beginGameButton").click(function() {
+  var roundCount = 0;
+  var humanScore = 0;
+  var compScore = 0;
+  
+  $(this).hide();
+  
+  function computerChoice () {
+  	var myNum = Math.floor(Math.random() * 3) + 1;
+    return myNum;
+  }
+
+	$(".gameButton").click(function() {
+  	if(roundCount++ === 5) {
+	    alert("The game has gone for five rounds! Human: " + humanScore + ". Comp: " + compScore);
+      $("#beginGameButton").show();
+      humanScore = compScore = roundCount = 0;
+      return;
+    }
+    var userChoice = parseInt($(this).val());
+    var compChoice = computerChoice();
+
+    if(compChoice === userChoice) {
+      alert("You got the right answer!");
+      humanScore++;
+    } else {
+      alert("You got the wrong answer. I picked " + compChoice);
+      compScore++;
+    }
+	});
 });
+
+
+
+
 
